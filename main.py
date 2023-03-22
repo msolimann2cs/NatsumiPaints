a8c6d1626aa4a01cb7e60724364193da78142ef7
msolimann2cs <msolimann2cs@gmail.com>
2023-03-22 10:17:21 +0200

    V0

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+# Define the list of image files
+images = []
+for filename in os.listdir('static/images'):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        images.append(filename)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', images=images)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+if __name__ == '__main__':
+    app.run()
